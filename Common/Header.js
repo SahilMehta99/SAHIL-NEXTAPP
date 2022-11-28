e4e74b47842b9e4d4f1ab32215c676df2183c96c
SahilMehta99 <118711389+SahilMehta99@users.noreply.github.com>
2022-11-28 14:10:06 +0530

    Add files via upload

--- Common/Header.js
+++ Common/Header.js
@@ -22,15 +22,14 @@ function Header() {
           >
            
           </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
+          <div className="col-md-3">
+        <Nav className="me-auto">
+        <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/logout">Log Out</Nav.Link>
+            <Nav.Link href="/register">Register</Nav.Link>
+            </Nav>
+
+        </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
